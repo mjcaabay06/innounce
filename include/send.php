31f2dd2ec9ce27251fdf13992a4103fb5fc11344
marccaabay <marc.caabay@paramount.com.ph>
2017-08-27 03:10:04 +0800

    fix sending message

--- include/send.php
+++ include/send.php
@@ -2,8 +2,8 @@
 	include("configurations.php");
 	include("general_functions.php");
 
-	sendViaBulksms('639176710089', 'This is just a test message.');
-	
+	$response = sendViaBulksms('639176710089', 'This is just a test message.');
+	insertMessage(1,'This is just a test message.',2,$response);
 
 	// $var = "639157559924";
 	// echo substr_replace($var, '0', 0, 2);
