5160dadeaec45c0a8c2a891bd5e4fbfe868d6990
marccaabay <marc.caabay@paramount.com.ph>
2017-08-26 08:22:01 +0800

    test receive

---
+++ include/receive.php
@@ -0,0 +1,21 @@
+<?php
+	include("configurations.php");
+	include("general_functions.php");
+
+	$msisdn = $_REQUEST['msisdn'];
+	$sender = $_REQUEST['sender'];
+	$message = $_REQUEST['message'];
+
+	$output = "A message with body " . $message . " was sent from " . $sender . " to " . $msisdn ."\n";
+	echo $output;
+	error_log($output);
+
+	// Hex-encoded unicode SMS bodies ($_REQUEST['dca']=='16bit') can be decoded to UTF-8
+	// (which is typically what you would want to use) with:
+	// $decoded_body = mb_convert_encoding(hex2bin($_REQUEST['message']), "UTF-8", "UTF-16");
+
+	// Print the rest of the pushed parameters:
+	foreach ( $_REQUEST as $param => $value ) {
+	  echo $param . ": " . $value . "<br />";
+	}
+?>
---
+++ include/send.php
@@ -0,0 +1,6 @@
+<?php
+	include("configurations.php");
+	include("general_functions.php");
+
+	sendViaBulksms('639176710089', 'This is just a test message.');
+?>
