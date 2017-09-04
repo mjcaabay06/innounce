45c0a33696460cedc6b2820e0bf9f7f7a35e8979
marccaabay <marc.caabay@paramount.com.ph>
2017-09-04 11:57:08 +0800

    fix timezone

--- partial/_access.php
+++ partial/_access.php
@@ -1,5 +1,4 @@
 <?php
-	date_default_timezone_set('Asia/Manila');
 	$checkLogs = "select * from login_logs inner join users on users.id = login_logs.user_id where login_logs.user_id = " . $userId . " and login_logs.status_id = 1 and users.password_type_id = 1";
 	$rsLogs = mysqli_query($mysqli, $checkLogs);
 	$rowLogs = mysqli_num_rows($rsLogs);
@@ -11,6 +10,21 @@
 	$checkLastLogout = "select * from logout_logs where user_id = " . $userId . " order by created_at desc limit 1,1";
 	$rsLastLogout = mysqli_query($mysqli, $checkLastLogout);
 	$rowLastLogout = mysqli_fetch_assoc($rsLastLogout);
+
+
+	$access_login = '';
+	if (isset($rowLastLogin)) {
+		$login = new DateTime($rowLastLogin['created_at']);
+		$login->setTimezone(new DateTimeZone('Asia/Manila'));
+		$access_login = 'Last Login' . $login->format("l, F j, Y") . '<br/>' . $login->format("g:i:s A (e O)");
+	}
+
+	$access_logout = '';
+	if (isset($rowLastLogout)) {
+		$logout = new DateTime($rowLastLogout['created_at']);
+		$logout->setTimezone(new DateTimeZone('Asia/Manila'));
+		$access_logout = 'Last Access' . $logout->format("l, F j, Y") . '<br/>' . $logout->format("g:i:s A (e O)");
+	}
 ?>
 <div class="row">
 
@@ -23,7 +37,7 @@
 							<div class="row">
 								<div class="col-xs-9 text-center pl-0 pr-0 data-wrap-left" style="min-height: 79px">
 									<span class="txt-light block font-13">
-										<?php echo isset($rowLastLogout) ? 'Last Access ' . date('l, F j, Y', strtotime($rowLastLogout['created_at'])) . '<br/>' . date('g:i:s A (e O)', strtotime($rowLastLogout['created_at'])) : '' ?>
+										<?php echo $access_logout ?>
 									</span>
 								</div>
 								<div class="col-xs-3 text-center  pl-0 pr-0 data-wrap-right" style="min-height: 79px">
@@ -45,7 +59,7 @@
 							<div class="row">
 								<div class="col-xs-9 text-center pl-0 pr-0 data-wrap-left" style="min-height: 79px">
 									<span class="txt-light block font-13">
-										<?php echo isset($rowLastLogin) ? 'Last Login ' . date('l, F j, Y', strtotime($rowLastLogin['created_at'])) . '<br/>' . date('g:i:s A (e O)', strtotime($rowLastLogin['created_at'])) : '' ?>
+										<?php echo $access_login ?>
 									</span>
 								</div>
 								<div class="col-xs-3 text-center  pl-0 pr-0 data-wrap-right" style="min-height: 79px">
--- worker/script.php
+++ worker/script.php
@@ -1,5 +1,14 @@
 <?php
-	date_default_timezone_set('Asia/Manila');
+	include("../include/configurations.php");
+	include("../include/general_functions.php");
+	//date_default_timezone_set('Pacific/Efate');
 
-	error_log("Hello: " . date('Y-m-d H:i:s',time()));
+	//error_log("Hello: " . date('Y-m-d H:i:s',time()));
+	$sel = "select * from login_logs order by created_at desc limit 1";
+	$rs = mysqli_query($mysqli, $sel);
+	$row = mysqli_fetch_assoc($rs);
+
+	$login = new DateTime($row['created_at']);
+	$login->setTimezone(new DateTimeZone('Asia/Manila'));
+	echo $login->format("Y-m-d H:i:sP") . "\n";
 ?>
