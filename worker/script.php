c02889305d2b44215d73a279fd8afb1c1db6547a
marccaabay <marc.caabay@paramount.com.ph>
2017-09-04 13:52:49 +0800

    text

--- worker/script.php
+++ worker/script.php
@@ -4,11 +4,11 @@
 	//date_default_timezone_set('Pacific/Efate');
 
 	//error_log("Hello: " . date('Y-m-d H:i:s',time()));
-	// $sel = "select * from login_logs order by created_at desc limit 1";
-	// $rs = mysqli_query($mysqli, $sel);
-	// $row = mysqli_fetch_assoc($rs);
+	$sel = "select * from login_logs order by created_at desc limit 1";
+	$rs = mysqli_query($mysqli, $sel);
+	$row = mysqli_fetch_assoc($rs);
 
-	// echo setDate("Y-m-d H:i:sP", $row['created_at']) . "\n";
+	echo setDate("Y-m-d H:i:sP", $row['created_at']) . "\n";
 
-	echo date_default_timezone_get();
+	//echo date_default_timezone_get();
 ?>
