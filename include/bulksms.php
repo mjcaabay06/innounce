321b3a94e0654a914ff3c58c827573d47a4647a7
marccaabay <marc.caabay@paramount.com.ph>
2017-09-06 10:54:24 +0800

    fix emergencies

--- include/bulksms.php
+++ include/bulksms.php
@@ -133,8 +133,8 @@ function seven_bit_sms ($message, $msisdn ) {
   'msisdn'   => $msisdn,
   'allow_concat_text_sms' => 0, # Change to 1 to enable long messages
   'concat_text_sms_max_parts' => 2,
-  'sender' => 'iNnounce'
-  //'repliable' => 0
+  'sender' => 'iNnounce',
+  'repliable' => 0
   );
 
   return make_post_body($post_fields);
