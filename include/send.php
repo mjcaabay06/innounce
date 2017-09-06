9922ce2207f464051bd751b426a7add6dfb304da
marccaabay <marc.caabay@paramount.com.ph>
2017-09-06 18:58:22 +0800

    test

--- include/send.php
+++ include/send.php
@@ -17,20 +17,39 @@
 	// }
 	// return send_message($post_body);
 
-	$hasYear = [1,2,3,4];
-	$course = 1;
+	// $hasYear = [1,2,3,4];
+	// $course = 1;
+	// $mobile = array();
+	// $aa = array();
+
+	// foreach ($hasYear as $year) {
+	// 	foreach (getStudentReceivers($year,$course) as $studNumber) {
+	// 		$aa['number'] = substr_replace($studNumber['mobile_number'], '63', 0, 1);
+	// 		$aa['id'] = $studNumber['student_id'];
+	// 		array_push($mobile, $aa);
+	// 	}
+	// }
+
+	// foreach ($mobile as $recipient) {
+	// 	echo $recipient['id'] . '<br/>';
+	// }
+
 	$mobile = array();
 	$aa = array();
-
-	foreach ($hasYear as $year) {
-		foreach (getStudentReceivers($year,$course) as $studNumber) {
-			$aa['number'] = substr_replace($studNumber['mobile_number'], '63', 0, 1);
-			$aa['id'] = $studNumber['student_id'];
-			array_push($mobile, $aa);
-		}
+	foreach(getAllReceivers() as $rcvr) {
+		//$mobile[] = substr_replace($rcvr['mobile_number'], '63', 0, 1);
+		$aa['number'] = substr_replace($rcvr['mobile_number'], '63', 0, 1);
+		$aa['id'] = $rcvr['recipient_id'];
+		array_push($mobile, $aa);
 	}
 
-	foreach ($mobile as $recipient) {
-		echo $recipient['id'] . '<br/>';
+	$imploded = implode(',', array_map(function($e){ return $e['number']; }, $mobile));
+	$response = sendViaBulksms($imploded, $message);
+	if( $result['success'] ) {
+		print_ln( formatted_server_response( $result ) );
+		//insertMessage(1,'This is just a test message.',2,$result);
+	}
+	else {
+		print_ln( formatted_server_response( $result ) );
 	}
 ?>
