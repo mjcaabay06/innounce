f021109331e335dd661dc3fdb7a46bbc83a94089
marccaabay <marc.caabay@paramount.com.ph>
2017-09-08 07:04:22 +0800

    fix sending

--- include/bulksms.php
+++ include/bulksms.php
@@ -126,13 +126,16 @@ function seven_bit_sms ($message, $msisdn ) {
   $post_fields = array (
   'username' => 'xcellence1',
   'password' => 'unonatayo1',
+  // 'username' => 'pilipinas5481',
+  // 'password' => 'sabert00th3693',
   //'message'  => character_resolve( $message ),
   'message'  => $message,
   'msisdn'   => $msisdn,
   'allow_concat_text_sms' => 0, # Change to 1 to enable long messages
   'concat_text_sms_max_parts' => 2,
-  'sender' => 'iNnounce',
-  'repliable' => 0
+  'sender' => 'iNnounce2',
+  'repliable' => 0,
+  'routing_group' => 1
   );
 
   return make_post_body($post_fields);
