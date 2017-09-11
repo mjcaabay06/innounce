a323987ba907d6360dc31112b99f09e6cdccb414
marccaabay <marc.caabay@paramount.com.ph>
2017-09-11 14:31:02 +0800

    test

--- test.php
+++ test.php
@@ -2,5 +2,11 @@
 	include("include/general_functions.php");
 
 	$response = sendViaChikka('639176710089', 'This is just a test message coming from Chikka');
-	print_r($response->status);
+	print_r($response);
+	echo '<br/>';
+	if ((int)$response->status == 200) {
+		echo 'success';
+	} else {
+		echo 'failed';
+	}
  ?>
