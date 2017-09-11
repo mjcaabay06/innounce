323957187b41eb00392cad5a33e3319611859bde
marccaabay <marc.caabay@paramount.com.ph>
2017-09-11 13:44:16 +0800

    revisions

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 	include("include/general_functions.php");
 
-	$response = sendViaChikka('639176710089,639053179446', 'This is just a test message coming from Chikka');
-	print_r($response);
+	$response = sendViaChikka('639176710089', 'This is just a test message coming from Chikka');
+	print_r($response['status']);
  ?>
