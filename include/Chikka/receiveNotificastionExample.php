f29d8b30273bad0b575b3d7ef4bce3b35c184abc
marccaabay <marc.caabay@paramount.com.ph>
2017-09-11 17:29:04 +0800

    test

--- include/Chikka/receiveNotificastionExample.php
+++ include/Chikka/receiveNotificastionExample.php
@@ -6,9 +6,9 @@
  */
 
 include('ChikkaSMS.php');
-$clientId = 'xxxxx';
-$secretKey = 'xxxxxx';
-$shortCode = 'xxxxxx';
+$clientId = 'c315accbb28da59c05a6c0b4bc3248be5037c5ccc6cfed219a7507cfc2b01dc8';
+$secretKey = '56f51f5c4a81dc64ba3e0d5be7bb1087c0cb299c5659355f68d7b4910c42e179';
+$shortCode = '292907886';
 $chikkaAPI = new ChikkaSMS($clientId, $secretKey, $shortCode);
 
 if($_POST){
