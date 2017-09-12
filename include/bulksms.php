995ec574d827b52840fdcbfc2b9bb6ba9559161a
marccaabay <marc.caabay@paramount.com.ph>
2017-09-12 08:35:34 +0800

    test

--- include/bulksms.php
+++ include/bulksms.php
@@ -132,7 +132,9 @@ function seven_bit_sms ($message, $msisdn ) {
   'message'  => $message,
   'msisdn'   => $msisdn,
   'allow_concat_text_sms' => 0, # Change to 1 to enable long messages
-  'concat_text_sms_max_parts' => 2
+  'concat_text_sms_max_parts' => 2,
+  'sender' => 'iNnounce2',
+  'routing_group' => 2
   );
 
   return make_post_body($post_fields);
